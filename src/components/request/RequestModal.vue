c155fd1ee57ea6403521f7c521631acfa0d9338d
manzaero <ligoero@gmail.com>
2023-01-11 18:26:16 +0500

    refact RequestModal

--- src/components/request/RequestModal.vue
+++ src/components/request/RequestModal.vue
@@ -1,9 +1,29 @@
 <template>
   <form>
     <div class="form-control">
-      <label for="">Ф И О</label>
-      <input type="text">
+      <label for="name">Ф И О</label>
+      <input type="text" id="name">
     </div>
+
+    <div class="form-control">
+      <label for="name">Телефон</label>
+      <input type="text" id="name">
+    </div>
+
+    <div class="form-control">
+      <label for="name">Сумма</label>
+      <input type="text" id="name">
+    </div>
+
+    <div class="form-control">
+      <label for="status">Статус</label>
+      <select name="status" id="status">
+        <option value="active">Активный</option>
+        <option value="ending">Закончен</option>
+        <option value="pending">В ожидании</option>
+      </select>
+    </div>
+    <button class="btn primary">Добавить</button>
   </form>
 </template>
 
