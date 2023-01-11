7a5338bf4f1a0334531505f7db1e2c31aa4f6879
manzaero <ligoero@gmail.com>
2023-01-11 17:55:51 +0500

    create RequestModal.vue

---
+++ src/components/request/RequestModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label for="">Ф И О</label>
+      <input type="text">
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "RequestModal"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <app-page title="Список">
     <template #header>
-      <button class="btn primary">Создать</button>
+      <button class="btn primary" @click="modal = true">Создать</button>
     </template>
     <request-table :requests="[]"></request-table>
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать" @close="modal = false">
+        <request-modal/>
+      </app-modal>
+    </teleport>
   </app-page>
 
   <app-footer info="test">
@@ -13,11 +19,20 @@
 </template>
 
 <script>
+import {ref} from 'vue'
 import AppPage from "@/components/ui/AppPage";
 import AppFooter from "@/components/ui/AppFooter";
 import RequestTable from "@/components/request/RequestTable";
+import AppModal from "@/components/ui/AppModal";
+import RequestModal from "@/components/request/RequestModal";
 export default {
-  name: 'Home',
-  components:{AppPage, AppFooter, RequestTable}
+  setup(){
+    const modal = ref(false)
+
+    return {
+      modal
+    }
+  },
+  components:{AppPage, AppFooter, RequestTable, AppModal, RequestModal}
 }
 </script>
