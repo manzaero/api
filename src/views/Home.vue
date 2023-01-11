0025c6bf8d85c45cf1b9e134b9448725c04d0bc7
manzaero <ligoero@gmail.com>
2023-01-11 17:27:56 +0500

    add requestTable.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <template #header>
       <button class="btn primary">Создать</button>
     </template>
+    <request-table :requests="[]"></request-table>
   </app-page>
 
   <app-footer info="test">
@@ -14,8 +15,9 @@
 <script>
 import AppPage from "@/components/ui/AppPage";
 import AppFooter from "@/components/ui/AppFooter";
+import RequestTable from "@/components/request/RequestTable";
 export default {
   name: 'Home',
-  components:{AppPage, AppFooter}
+  components:{AppPage, AppFooter, RequestTable}
 }
 </script>
