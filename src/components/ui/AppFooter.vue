c7f89fa7e8fb0d9571c0cf8452d1c074c9f8b9fc
manzaero <ligoero@gmail.com>
2023-01-13 11:37:14 +0500

    get api testing in AppFooter

--- src/components/ui/AppFooter.vue
+++ src/components/ui/AppFooter.vue
@@ -3,10 +3,23 @@
     <div class="card-title">
       {{test}}
     </div>
+    <hr>
+    <div class="card" v-for="user in users" :key="user.id">
+      <div class="card">
+        <ul>
+          <li>{{user.name}}</li>
+          <li>{{user.username}}</li>
+          <li>{{user.email}}</li>
+        </ul>
+      </div>
+    </div>
+    <button class="btn danger" @click="getUsers">Load</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props:{
     test:{
@@ -14,8 +27,20 @@ export default {
       required: true
     }
   },
-  setup(props){
-    document.test = `${props.test}`
+  data(){
+    return {
+      users: []
+    }
+  },
+  methods:{
+    async getUsers(){
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        this.users = response.data
+      } catch (e) {
+        console.error(e)
+      }
+    }
   }
 }
 </script>
