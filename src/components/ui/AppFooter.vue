1e682caf4cb1f9a335034f253c3b9bb6f2ff720e
manzaero <ligoero@gmail.com>
2023-01-16 22:11:40 +0500

    refact AppFooter.vue

--- src/components/ui/AppFooter.vue
+++ src/components/ui/AppFooter.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="card">
-    <div class="card-title">
-      {{test}}
-    </div>
-    <hr>
-    <div class="card" v-for="user in users" :key="user.id">
-      <div class="card">
-        <ul>
-          <li>Name: {{user.name}}</li>
-          <li>Username: {{user.username}}</li>
-          <li>E-mail: {{user.email}}</li>
-        </ul>
-      </div>
-    </div>
-    <button class="btn danger" @click="getUsers">Load</button>
-  </div>
+	<div class="card">
+		<div class="card-title">
+			{{ test }}
+		</div>
+		<hr>
+		<div class="card" v-for="user in users" :key="user.id">
+			<div class="card">
+				<ul>
+					<li>Name: {{ user.name }}</li>
+					<li>Username: {{ user.username }}</li>
+					<li>E-mail: {{ user.email }}</li>
+				</ul>
+			</div>
+		</div>
+		<button class="btn danger" @click="getUsers">Load</button>
+	</div>
 </template>
 
 <script>
@@ -22,36 +22,27 @@ import axios from "axios";
 import store from "@/store";
 
 export default {
-  props:{
-    test:{
-      type: String,
-      required: true
-    }
-  },
-  data(){
-    return {
-      users: []
-    }
-  },
-  methods:{
-    async getUsers(){
-      try {
-        const token = {
-          headers: {Authorization: `Bearer ${store.state["auth/token"]}`}
-        };
-        const params = {
-          key: value
-        }
-        const response = await axios.get('https://test.octopus.uz/api/v1/users',
-            this.token)
-        // this.users = response.data
-        console.log(params)
-        console.log(response)
-        } catch (e) {
-        console.error(e)
-      }
-    }
-  }
+	props: {
+		test: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			users: []
+		}
+	},
+	methods: {
+		async getUsers() {
+			try {
+				const users = await axios.get('https://jsonplaceholder.typicode.com/users');
+				this.users = users
+			} catch (e) {
+
+			}
+		}
+	}
 }
 </script>
 
