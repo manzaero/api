396a81febc2bef947ca7ca7b7759cdbc736627b1
manzaero <ligoero@gmail.com>
2025-08-08 23:24:00 +0300

    refact ...

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: '/port-api',
+  publicPath: '/port-api/',
   devServer: {
     hot: false
   }
